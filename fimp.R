4d68b9f61cc0c4fbd2c9f077057ed0f15e63b7c4
Daniel <2005danielus@gmail.com>
2022-10-23 14:50:41 +0200

    Create fimp.R

---
+++ fimp.R
@@ -0,0 +1,49 @@
+# Create Twitter token
+fewcoordinates_token <- rtweet::rtweet_bot(
+  api_key       = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+  api_secret    = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+  access_token  = Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
+
+# Generate random coordinates within specific limits
+lon <- round(runif(1, 93.342, 151.3811), 4)
+lon <- format(lon, scientific = FALSE)
+lat <- round(runif(1, -14.7967, 23.0782), 4)
+
+# Build URL and fetch image from Mapbox API
+img_url <- paste0(
+  "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/",
+  paste0(lon, ",", lat),
+  ",15,0/600x400@2x?access_token=",
+  Sys.getenv("MAPBOX_PUBLIC_ACCESS_TOKEN")
+)
+
+# Download the image to a temporary location
+temp_file <- tempfile(fileext = ".jpeg")
+download.file(img_url, temp_file)
+
+# Build the status message (text and URL)
+latlon_details <- paste0(
+  "ℹ️: ", "Filipinas, Indonesia, Malasia, Papúa Nueva Guinea/Philippines, Indonesia, Malaysia, Papua New Guinea (FIMP)", "\n",
+  "📍: ", lat, ", ", lon, "\n",
+  "🗺: ",  paste0("https://www.openstreetmap.org/",
+                  "?mlat=", lat,
+                  "&mlon=", lon,
+                  "#map=17/", lat, "/", lon)
+)
+
+# Provide alt-text description
+alt_text <- paste(
+  "A satellite image of a random location in the world,",
+  "provided by MapBox. Typically contains a residential or",
+  "industrial area."
+)
+
+# Post the image to Twitter
+rtweet::post_tweet(
+  status         = latlon_details,
+  media          = temp_file,
+  media_alt_text = alt_text,
+  token          = fewcoordinates_token
+)
